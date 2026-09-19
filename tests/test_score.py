35abc4565c851f2f149c9be3fda1d577c1144473
brwali <brandon007walia@gmail.com>
2026-09-19 16:50:04 -0400

    feat: cleaned up code based on comments
    * Big thing is using global scorer so models are only downloaded once
    * Also added the score to /ask message
    * Made everything snake case

--- tests/test_score.py
+++ tests/test_score.py
@@ -1,21 +1,21 @@
 import numpy as np
-
+import pytest
 from core.score import score
 
 
 def test_score_combines_cross_encoder_and_cosine_similarity():
-    input = "I am writing a unit test and I need your output to make it real"
-    output = "Something you need to buy a PC until I figure out where I'm working"
-    assert score(input, output) == 14.84
+    input_text = "I am writing a unit test and I need your output to make it real"
+    output_text = "Something you need to buy a PC until I figure out where I'm working"
+    assert score(input_text, output_text) == pytest.approx(14.84, abs=0.5)
 
-    input = "retep I need you to be locked in and give me a good response"
-    output = "Good at when it comes down to it"
-    assert score(input, output) == 14.9
+    input_text = "retep I need you to be locked in and give me a good response"
+    output_text = "Good at when it comes down to it"
+    assert score(input_text, output_text) == pytest.approx(14.9, abs=0.5)
 
-    input = "who is the diddy blud"
-    output = "In the diddy acolyte's basement?"
-    assert score(input, output) == 16.47
+    input_text = "who is the diddy blud"
+    output_text = "In the diddy acolyte's basement?"
+    assert score(input_text, output_text) == pytest.approx(16.47, abs=0.5)
     # This is a non retep example to show that the scoring actually can go high
-    input = "What is the capital of France"
-    output = "The capital of France is Paris"
-    assert score(input, output) == 99.53
+    input_text = "What is the capital of France"
+    output_text = "The capital of France is Paris"
+    assert score(input_text, output_text) == pytest.approx(99.53, abs=0.5)
